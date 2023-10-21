acdda772c1dcc7f310be0914123f5a298ea7c92a
Nikolas Raposo Martins Vieira <62063505+NikolasVieira@users.noreply.github.com>
2023-10-20 22:19:18 -0300

    Inicio do Projeto

---
+++ Models/ProdutoCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoPOO.Models
+{
+    public class ProdutoCollection : List<Produto>
+    {
+
+    }
+}
---
+++ Services/DataBaseSqlServer.cs
@@ -0,0 +1,25 @@
+﻿//É preciso importar as bibliotecas do SQLSERVER
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ProjetoPOO.Services
+{
+    //Métodos Privado que cria nova conexão com o banco
+    private SqlConnection CriarConexao()
+    {
+        SqlConnection conexao = new SqlConnection();
+
+        conexao.ConnectionString = 
+            "Data Source = localhost;" +
+            "Initial Catalog = pooCamadas;" +
+            "Integrated Security = SSPI;";
+        conexao.Open();
+        return conexao;
+    }
+
+    public class DataBaseSqlServer
+    {
+        
+    }
+}
